4c7fb7fc5ceb785354983ec5cee82681ab73625a
junior_zhu <yunerzhu@gmail.com>
2018-12-11 14:10:06 +0800

    Update contact_me.js

--- js/contact_me.js
+++ js/contact_me.js
@@ -47,7 +47,7 @@ $(function() {
                     $('#success').html("<div class='alert alert-danger'>");
                     $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                         .append("</button>");
-                    $('#success > .alert-danger').append($("<strong>").text("Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later! Or directly contact instructor via yunerzhu@gmail.com"));
+                    $('#success > .alert-danger').append($("<strong>").text("Sorry " + firstName + ", it seems that my mail server is not responding. Please try contacting instructor via yunerzhu@gmail.com directly!"));
                     $('#success > .alert-danger').append('</div>');
                     //clear all fields
                     $('#contactForm').trigger("reset");

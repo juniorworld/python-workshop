44e9d2097b245879cd6771dfb4418ca034e410fd
junior_zhu <yunerzhu@gmail.com>
2018-12-11 14:24:38 +0800

    Update contact_me.php

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -20,6 +20,6 @@ $to = 'yunerzhu@gmail.com'; // Add your email address inbetween the '' replacing
 $email_subject = "Website Contact Form:  $name";
 $email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
 $headers = "From: noreply@2018pythonworkshop.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com. 
-mail($to,$email_subject,$email_body,$headers);
+#mail($to,$email_subject,$email_body,$headers);
 return true;         
 ?>

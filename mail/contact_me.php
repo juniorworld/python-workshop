87a81a8f714c78d9a6149adceb16d885cd2a3425
junior_zhu <yunerzhu@gmail.com>
2023-01-06 20:14:44 +0800

    Add files via upload

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -16,10 +16,10 @@ $phone = strip_tags(htmlspecialchars($_POST['phone']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
    
 // Create the email and send the message
-$to = 'yunerzhu@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+$to = 'yunerzhu@hkbu.edu.hk'; 
 $email_subject = "Website Contact Form:  $name";
 $email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
-$headers = "From: noreply@2018pythonworkshop.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com. 
+$headers = "From: noreply@pythonworkshop.com\n"; 
 mail($to,$email_subject,$email_body,$headers);
 return true;         
 ?>
